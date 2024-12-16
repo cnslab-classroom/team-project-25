9514df00dd5347bc528b71731663ac00ca77c2ff
iceisice <iceisice@naver.com>
2024-12-16 13:25:17 +0900

    불필요 내용 삭제

--- RealTimeTransactionProcessor.java
+++
@@ -1,8 +0,0 @@
-// 실시간으로 비동기 거래를 처리하는 스레드 기반 모듈
-
-class RealTimeTransactionProcessor {
-    public void processTransaction(Runnable transactionTask) {
-        Thread transactionThread = new Thread(transactionTask);
-        transactionThread.start();
-    }
-}
